434463b53e3ee52b94562ab2d732dfb9c96c4293
awallace87 <andrew.f.wallace@gmail.com>
2015-03-09 02:38:11 -0400

    Calculation of Mobility Score Added

--- assignment2/dev/project/Assets/Game/scripts/command/InitializeGameBoardCommand.cs
+++ assignment2/dev/project/Assets/Game/scripts/command/InitializeGameBoardCommand.cs
@@ -5,17 +5,56 @@ using System;
 
 public class InitializeGameBoardCommand : Command {
 
-	public override void Execute ()
-	{
-		IGameManager gameManager = injectionBinder.GetInstance<IGameManager> () as IGameManager;
+    [Inject]
+    public IGameManager gameManager { get; set; }
 
+    public override void Execute ()
+	{
 		gameManager.GetGameBoard ().Board = new IBoardSquareModel[OthelloConstants.StandardBoardSize, OthelloConstants.StandardBoardSize];
 
 		for (uint i = 0; i < OthelloConstants.StandardBoardSize; i++) {
 			for (uint j = 0; j < OthelloConstants.StandardBoardSize; j++) {
 				gameManager.GetGameBoard().Board[i,j] = injectionBinder.GetInstance<IBoardSquareModel>() as IBoardSquareModel;
-				gameManager.GetGameBoard().Board[i,j].MobilityScore = 1;
+				uint mobilityScore = GetMobilityScore(i,j);
+                gameManager.GetGameBoard().Board[i,j].MobilityScore = mobilityScore;
+                //Debug.Log("Initialize Value for + " + i + "," + j + " - " + mobilityScore);
 			}
 		}
 	}
+
+    private uint GetMobilityScore(uint x, uint y)
+    {
+        uint weightScale = gameManager.GetGameBoard().BoardSize;
+
+        uint xDist, yDist;
+        //Distance to closest x edge
+        if (x <= weightScale / 2)
+        {
+            xDist = x;
+        }
+        else
+        {
+            xDist = weightScale - x - 1;
+        }
+
+        if (y <= weightScale / 2)
+        {
+            yDist = y;
+        }
+        else
+        {
+            yDist = weightScale - y - 1;
+        }
+
+        int totalDistance = (int)Math.Min(xDist, yDist);
+
+        //Give corners extra value
+        if ((x == 0 || x == (weightScale - 1)) && (y == 0 || y == (weightScale - 1))) 
+        {
+            totalDistance--;
+        }
+
+
+        return (uint)Math.Pow(2, (weightScale / 2.0) - totalDistance);
+    }
 }
