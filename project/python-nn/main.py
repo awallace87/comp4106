c094ed11c588e8f838b6f8e866ee47d45a9d0279
awallace87 <andrew.f.wallace@gmail.com>
2015-04-19 17:59:00 -0400

    Added Fantasy Hockey Prediction

--- project/python-nn/main.py
+++ project/python-nn/main.py
@@ -1,44 +1,145 @@
 import pandas as pd
 import numpy as np
 import neuron as neuron
+from enum import Enum
 import random
 
+class PlayoffPosition(Enum):
+    Champ = 0
+    Finalist = 1
+    Conference = 2
+    Semi = 3
+    Quarter = 4
 
 #Playoff Prediction
 PLAYOFF_EPOCH = 5
+def make_playoff_position_preds():
+    playoff_df = pd.read_csv('playoffs.csv')
+    playoff_norm_df = (playoff_df - playoff_df.mean()) / (playoff_df.max() - playoff_df.min())
 
-playoff_df = pd.read_csv('playoffs.csv')
-playoff_norm_df = (playoff_df - playoff_df.mean()) / (playoff_df.max() - playoff_df.min())
+    num_inputs = len(playoff_norm_df.columns) - 5
+    playoff_class = neuron.NeuralNetwork(num_inputs, 1, 5)
 
-num_inputs = len(playoff_norm_df.columns) - 5
-playoff_class = neuron.NeuralNetwork(num_inputs, 1, 5)
-
-playoff_norm_df.iloc[:,0:5] = playoff_df.iloc[:,0:5]
-print(playoff_norm_df.iloc[:, 6:(num_inputs + 5)])
-
-
-#Add in multiple epoch training
-epoch_num = 0
-index_sequence = range(len(playoff_norm_df))
-while epoch_num < PLAYOFF_EPOCH:
-    random.shuffle(index_sequence)
-    for i in index_sequence:
-        row = playoff_norm_df.iloc[i]
-        playoff_class.train(row[6:(num_inputs + 5)], row[0:5])
-    epoch_num += 1
-    print("Finished Epoch")
-
-#cross-validation
-
-#predict for latest season
+    print(playoff_norm_df.columns[:5])
+    playoff_norm_df.iloc[:,:5] = playoff_df.iloc[:,:5]
+    print(playoff_norm_df.iloc[:, 5:])
 
 
-#NEW PREDICTIONS TO MODEL
-VALID_SEASON_YEARS = ["2015","2014","2013","2011"]
-hockeyref_filename = "HockeyReference_skater_{0}_season"
+    #Add in multiple epoch training
+    epoch_num = 0
+    index_sequence = range(len(playoff_norm_df))
+    index_len = len(index_sequence)
+    split_ind = int(index_len * 0.8)
+    total_tests = 0.0
+    total_success = 0.0
+    while epoch_num < PLAYOFF_EPOCH:
+        random.shuffle(index_sequence)
+        train_list = index_sequence[:split_ind]
+        test_list = index_sequence[(-split_ind):]
+        total_tests += len(test_list)
+        for i in train_list:
+            row = playoff_norm_df.iloc[i]
+            playoff_class.train(row[5:], row[:5])
+        for j in test_list:
+            row = playoff_norm_df.iloc[j]
+            results = playoff_class.test(row[5:])
+            actual = list(row[:5])
+            #print(results)
+            if(results.index(max(results)) == actual.index(max(actual))):
+                total_success += 1
+                #print("Success")
+        epoch_num += 1
+        print("Finished Epoch")
 
-#gather dataframes
-master_data_frame_collection = []
+    #cross-validation
+    success_rate = float(total_success / total_tests)
+    print("Overall Rate of Success: {0}".format(total_success / total_tests))
 
-for year in VALID_SEASON_YEARS:
-    print(hockey_ref_filename.format(year))
+    #predict for latest season
+    current_season_df = pd.read_csv("currentseason.csv")
+    norm_cols = list(current_season_df.columns[1:])
+
+    print("Current Season Cols - {0}".format(len(current_season_df.columns[1:])))
+    print("Regular Cols - {0}".format(len(playoff_norm_df.columns[5:])))
+
+
+    for col in norm_cols:
+        current_season_df[col] = (current_season_df[col] - current_season_df[col].mean()) / (current_season_df[col].max() - current_season_df[col].min())
+
+    current_season_df = current_season_df.fillna(-0.015625)
+
+    print(current_season_df)
+
+    for index, row in current_season_df.iterrows():
+        result = playoff_class.test(row[1:])
+        enum_pos = PlayoffPosition(result.index(max(result)))
+        print(row[0] + " - Predicted Playoff Position = {0}, with prob - {1}".format(enum_pos, max(result)))
+
+PLAYER_FANTASY_EPOCH = 2
+
+def make_fantasy_player_preds():
+
+    hockeyref_fantasy_file = "hockeyref_combine_player_fantasy.csv"
+    player_fantasy_df = pd.read_csv(hockeyref_fantasy_file)
+    player_fantasy_df.fillna(value=0.0)
+
+    num_inputs = len(player_fantasy_df.columns)
+
+    norm_fantasy_cols = list(player_fantasy_df.columns[6:].values)
+    norm_fantasy_cols.remove("Year")
+    norm_fantasy_cols.append("Age")
+
+    num_train_inputs = len(norm_fantasy_cols)
+
+    player_norm_df = player_fantasy_df
+    for col in norm_fantasy_cols:
+        player_norm_df[col] = (player_fantasy_df[col] - player_fantasy_df[col].mean()) / (player_fantasy_df[col].max() - player_fantasy_df[col].min())
+
+    player_epoch_num = 0
+    index_sequence = range(len(player_norm_df))
+
+    fantasy_class = neuron.NeuralNetwork(num_train_inputs, 1, 1)
+
+    print(norm_fantasy_cols)
+
+    index_len = len(index_sequence)
+    split_ind = int(index_len * 0.8)
+    total_tests = 0.0
+    total_success = 0.0
+
+    while player_epoch_num < PLAYER_FANTASY_EPOCH:
+        random.shuffle(index_sequence)
+        for i in index_sequence[:split_ind]:
+            row = player_norm_df.iloc[i]
+            row_name = row["Player"]
+            row_year = row["Year"]
+            next_year_df = player_norm_df[(player_norm_df["Player"] == row_name) | (player_norm_df["Year"] == (row_year + 1))]
+            if len(next_year_df) > 0:
+                #print("Found Next Year")
+                first_entry = next_year_df.iloc[0]
+                #print(first_entry["Fantasy"])
+                fantasy_class.train(row[6:], [(first_entry["Fantasy"])])
+        for j in index_sequence[split_ind:]:
+            row = player_norm_df.iloc[j]
+            row_name = row["Player"]
+            row_year = row["Year"]
+            next_year_df = player_norm_df[(player_norm_df["Player"] == row_name) | (player_norm_df["Year"] == (row_year + 1))]
+            if len(next_year_df) > 0:
+                total_tests += 1
+                first_entry = next_year_df.iloc[0]
+                result_fantasy = fantasy_class.test(row[6:])
+                diff_fantasy = float(result_fantasy[0]) - float(first_entry["Fantasy"])
+                print("Fantasy Test, Expected Val = {0}, Result = {1}".format(float(first_entry["Fantasy"]), result_fantasy))
+                if(diff_fantasy < 0.1 and diff_fantasy > -0.1):
+                    #print("Success")
+                    total_success += 1
+        print("Epoch Complete")
+        player_epoch_num += 1
+
+    print("Fantasy Predictions Success Rate = {0}".format(total_success / total_tests))
+    #fantasy_class.print_network()
+
+
+if __name__ == "__main__":
+    #make_playoff_position_preds()
+    make_fantasy_player_preds()
